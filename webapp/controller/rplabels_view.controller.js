d395c07beec9ee230cfb320c6cbc248e44b81a6a
Venkata Manohar Gedala <venkatamanohar.gedala@answerthink.com>
2025-02-22 19:56:29 +0000

    rp labels 0.2

--- webapp/controller/rplabels_view.controller.js
+++ webapp/controller/rplabels_view.controller.js
@@ -22,13 +22,17 @@ sap.ui.define([
     SearchField, UIColumn, MColumn, Label, TypeString, compLibrary, FilterOperator, Fragment, Message) => {
     "use strict";
     var addDefaultEntries = [];
-    var addEntriesLength = 0,oBusyDialog;
-    var oAddEntryModel,omaterialsModel;
-    var i;
+    var addEntriesLength = 0, oBusyDialog, oAutoSearchDialog;
+    var oAddEntryModel, omaterialsModel = new sap.ui.model.json.JSONModel(), oallmaterialsModel = new sap.ui.model.json.JSONModel(),ouserModel = new sap.ui.model.json.JSONModel();
+    var i, oMatInput;
     var ValueState = coreLibrary.ValueState;
     return Controller.extend("rplabels.controller.rplabels_view", {
         onInit() {
             this._add_initial_entries();
+        },
+        onBeforeRendering(){
+            var oModel = this.getOwnerComponent().getModel();
+            this._getuserdetails(oModel);
         },
         _add_initial_entries: function () {
             var that = this;
@@ -43,6 +47,23 @@ sap.ui.define([
             oAddEntryModel.setData(addDefaultEntries);
             that.getView().setModel(oAddEntryModel, "Entries");
             oAddEntryModel.refresh(true);
+        },
+        _getuserdetails: function (oModel) {
+            oModel.read("/CurrentUser", {
+                success: function (oData) {
+                    var aUser = oData.results;
+                    var that = this;
+                    if (aUser.length > 0) {
+                        ouserModel.setData(aUser);
+                        that.getView().setModel(ouserModel);
+                        that.getView().bindElement("/0");
+                        console.log(aUser[0].jobname);
+                    } else {
+                    }
+                }.bind(this),
+                error: function (oError) {
+                }.bind(this)
+            });
         },
         /*Plant Value Help*/
         onPlantVH: function (oEvent) {
@@ -103,29 +124,27 @@ sap.ui.define([
             var that = this,
                 aTokens = oEvent.getParameter("tokens"),
                 oModel = this.getView().getModel();
-            omaterialsModel = new sap.ui.model.json.JSONModel();
-            oBusyDialog = new sap.m.BusyDialog({
-                title: "Loading Materials",
-                text: "Please wait....."
-            });
-            oBusyDialog.open();
             if (aTokens.length > 0) {
+                var oFilter = [],
+                    oPlantfilter;
                 var oText = aTokens[0].getKey();
                 var oselectedSet = aTokens[0].getAggregation("customData");
                 var oselectedData = oselectedSet[0].getProperty("value");
                 var oPlant = this.byId("id_plant");
                 oPlant.setValue(oText);
-                let oMaterials = await that._getPlantmaterials(oModel, oText);
-                if(oMaterials){
-                    omaterialsModel.setData(oMaterials);
-                    that.getView().setModel(omaterialsModel, "Materials");
-                    oBusyDialog.close();
-                }else{
-                    omaterialsModel.setData(oMaterials);
-                    omaterialsModel.refresh();
-                    that.getView().setModel(omaterialsModel, "Materials");
-                    oBusyDialog.close();
-                }
+                /*   oPlantfilter = new sap.ui.model.Filter("Plant", "EQ", oText);
+                   oFilter.push(oPlantfilter)
+                   let oMaterials = await that._getPlantmaterials(oModel, oFilter);
+                   if (oMaterials) {
+                       oallmaterialsModel.setData(oMaterials);
+                       that.getView().setModel(oallmaterialsModel, "AllMaterials");
+                       oBusyDialog.close();
+                   } else {
+                       oallmaterialsModel.setData(oMaterials);
+                       oallmaterialsModel.refresh();
+                       that.getView().setModel(oallmaterialsModel, "AllMaterials");
+                       oBusyDialog.close();
+                   } */
             }
             this._oVHPlantWithSuggestions.close();
         },
@@ -176,25 +195,58 @@ sap.ui.define([
         },
         onPlantChange: async function (oEvent) {
             var that = this,
+                oFilter = [],
+                oPlantfilter,
                 oValidatedPlant = oEvent.getSource(),
                 sSelectedKey = oEvent.getParameter("newValue"),
                 oModel = this.getView().getModel();
-                omaterialsModel = new sap.ui.model.json.JSONModel();
+            oModel.setSizeLimit(999999);
             let oPlant = await that._plantValidation(oModel, sSelectedKey);
             if (sSelectedKey !== oPlant) {
                 oValidatedPlant.setValueState(ValueState.Error);
                 oValidatedPlant.setValueStateText("Please enter a valid Plant!");
             } else {
                 oValidatedPlant.setValueState(ValueState.None);
-                let oMaterials = await that._getPlantmaterials(oModel, sSelectedKey);
-                if(oMaterials){
-                    omaterialsModel.setData(oMaterials);
-                    that.getView().setModel(omaterialsModel, "Materials");
-                }else{
-                    omaterialsModel.setData(oMaterials);
-                    omaterialsModel.refresh();
-                    that.getView().setModel(omaterialsModel, "Materials");
-                }
+                /* oPlantfilter = new sap.ui.model.Filter("Plant", "EQ", oPlant);
+                 oFilter.push(oPlantfilter);
+                 let oMaterials = await that._getPlantmaterials(oModel, oFilter);
+                 if (oMaterials) {
+                     oallmaterialsModel.setData(oMaterials);
+                     that.getView().setModel(oallmaterialsModel, "AllMaterials");
+                 } else {
+                     oallmaterialsModel.setData(oMaterials);
+                     oallmaterialsModel.refresh();
+                     that.getView().setModel(oallmaterialsModel, "AllMaterials");
+                 } */
+
+            }
+        },
+        onMaterialliveChange: async function (oEvent) {
+            var oModel = this.getView().getModel(),
+                oPlant = this.getView().byId("id_plant").getValue(),
+                sNewValue = oEvent.getParameter("value"),
+                that = this,
+                oFilter = [],
+                oPlantfilter,
+                oMaterialfilter;
+            oMaterialfilter = new sap.ui.model.Filter({
+                filters: [
+                    new sap.ui.model.Filter("Material", sap.ui.model.FilterOperator.Contains, sNewValue),
+                    new sap.ui.model.Filter("MaterialText", sap.ui.model.FilterOperator.Contains, sNewValue)
+                ],
+                and: false
+            });
+            oFilter.push(oMaterialfilter);
+            oPlantfilter = new sap.ui.model.Filter("Plant", "EQ", oPlant);
+            oFilter.push(oPlantfilter);
+            let oMaterials = await that._getPlantmaterials(oModel, oFilter);
+            if (oMaterials) {
+                omaterialsModel.setData(oMaterials);
+                that.getView().setModel(omaterialsModel, "Materials");
+            } else {
+                omaterialsModel.setData(oMaterials);
+                omaterialsModel.refresh();
+                that.getView().setModel(omaterialsModel, "Materials");
             }
         },
         _plantValidation: function (oModel, sInputValue) {
@@ -215,10 +267,11 @@ sap.ui.define([
                 });
             });
         },
-        _getPlantmaterials: function (oModel, sInputValue){
+        _getPlantmaterials: function (oModel, filters) {
             return new Promise((resolve, reject) => {
                 oModel.read("/Materials", {
-                    filters: [new sap.ui.model.Filter("Plant", sap.ui.model.FilterOperator.EQ, sInputValue)],
+                    filters: filters,
+                    urlParameters: { "$top": 200 },
                     success: function (oData) {
                         var aMaterials = oData.results;
                         if (aMaterials.length > 0) {
@@ -232,6 +285,118 @@ sap.ui.define([
                     }
                 });
             });
+        },
+        onMaterialVH: async function (oEvent) {
+            oMatInput = oEvent.getSource();
+            var sInputValue = oEvent.getSource().getValue(),
+                oPlant = this.getView().byId("id_plant").getValue(),
+                oView = this.getView(),
+                oFilter = [],
+                oPlantfilter,
+                oModel = this.getView().getModel(),
+                that = this;
+
+            oPlantfilter = new sap.ui.model.Filter("Plant", "EQ", oPlant);
+            oFilter.push(oPlantfilter);
+            let oMaterials = await that._getPlantmaterials(oModel, oFilter);
+            if (oMaterials) {
+                oallmaterialsModel.setData(oMaterials);
+                oallmaterialsModel.refresh();
+                that.getView().setModel(oallmaterialsModel, "AllMaterials");
+            } else {
+                oallmaterialsModel.setData(oMaterials);
+                oallmaterialsModel.refresh();
+                that.getView().setModel(oallmaterialsModel, "AllMaterials");
+            }
+            if (!this._pValueHelpDialog) {
+                this._pValueHelpDialog = Fragment.load({
+                    id: oView.getId(),
+                    name: "rplabels.view.MaterialVH",
+                    controller: this
+                }).then(function (oDialog) {
+                    oView.addDependent(oDialog);
+                    return oDialog;
+                });
+            }
+            this._pValueHelpDialog.then(function (oDialog) {
+                // Create a filter for the binding
+                if (sInputValue) {
+                    oDialog.getBinding("items").filter([new Filter("Material", FilterOperator.Contains, sInputValue)]);
+                    // Open ValueHelpDialog filtered by the input's value
+                    oDialog.open(sInputValue);
+                } else {
+                    oDialog.open();
+                }
+            });
+        },
+        onValueHelpmaterialSearch: async function (oEvent) {
+            var oFilter = [], oPlantfilter, oMaterialfilter,
+                oPlant = this.getView().byId("id_plant").getValue(),
+                oModel = this.getView().getModel(),
+                sValue = oEvent.getParameter("value"),
+                that = this;
+            if (sValue) {
+                oMaterialfilter = new sap.ui.model.Filter({
+                    filters: [
+                        new sap.ui.model.Filter("Material", sap.ui.model.FilterOperator.Contains, sValue),
+                        new sap.ui.model.Filter("MaterialText", sap.ui.model.FilterOperator.Contains, sValue)
+                    ],
+                    and: false
+                });
+                oFilter.push(oMaterialfilter);
+            }
+            oPlantfilter = new sap.ui.model.Filter("Plant", "EQ", oPlant);
+            oFilter.push(oPlantfilter);
+            let oMaterials = await that._getPlantmaterials(oModel, oFilter);
+            if (oMaterials) {
+                oallmaterialsModel.setData(oMaterials);
+                oallmaterialsModel.refresh();
+                that.getView().setModel(oallmaterialsModel, "AllMaterials");
+            } else {
+                oallmaterialsModel.setData(oMaterials);
+                oallmaterialsModel.refresh();
+                that.getView().setModel(oallmaterialsModel, "AllMaterials");
+            }
+
+        },
+        onValueHelpmaterialClose: function (oEvent) {
+            var oSelectedItem = oEvent.getParameter("selectedItem");
+            if (oSelectedItem) {
+                var oValue = oSelectedItem.getDescription();
+                oMatInput.setValue(oValue);
+            }
+
+        },
+        onAddEntry: function (oEvent) {
+            var that = this;
+            var addDefaultEntry = {};
+            oAddEntryModel = new sap.ui.model.json.JSONModel();
+            i = i + 1;
+            addDefaultEntry.Id = i;
+            addDefaultEntry.Label = '';
+            addDefaultEntry.Material = '';
+            addDefaultEntries.push(addDefaultEntry);
+            oAddEntryModel.setData(addDefaultEntries);
+            that.getView().setModel(oAddEntryModel, "Entries");
+            oAddEntryModel.refresh(true);
+        },
+        deleteRow: function (oEvent) {
+            var that;
+            that = this;
+            var getRemoveIndex = oEvent.getParameter("listItem");
+            var oCells = getRemoveIndex.getCells();
+            var sId = oCells[0].getValue();
+            var iId = sId * 1;
+            oAddEntryModel = new sap.ui.model.json.JSONModel();
+            const index = addDefaultEntries.findIndex((obj) => (obj.Id) === iId);
+            if (index !== -1) {
+                addDefaultEntries.splice(index, 1);
+                oAddEntryModel.setData(addDefaultEntries);
+                that.getView().setModel(oAddEntryModel, "Entries");
+                oAddEntryModel.refresh(true);
+            }
+            var oTable = this.getView().byId("tableId1");
+            oTable.removeItem(oEvent.getParameter("listItem"));
         }
 
     });
